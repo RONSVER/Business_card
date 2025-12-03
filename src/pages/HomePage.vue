780118ff27ef318c5fb4e0e105dd9d866dfe77ce
Ibragim.Chekmazov <ronsver123@gmail.com>
2025-12-03 18:50:13 +0100

    fix the tailwind and add header

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { AppHeader } from '@/widgets/app-header'
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
---
+++ src/widgets/app-header/ui/AppHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="p-10 bg-red-500 text-white">Tailwind работает!</div>
+</template>
